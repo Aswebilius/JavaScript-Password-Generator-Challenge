02efc1b9aa2036d6587fb5c1a540cb86e75f686b
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-05-13 16:04:32 -0400

    prompts created

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,23 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //function passwordLength(){
 
-}
+  }
+
+  function generatePassword(){
+      passwordLength = prompt("Please type in Password Character Length (must be no less than 8 characters and no more than 128 characters):");
+      upperCaseChar = confirm("Accept Uppercase Characters?");
+      lowerCaseChar = confirm("Accept Lowercase Characters?");
+      numericChar = confirm("Accept Numeric Characters?");
+      specialChar = confirm("Accept Special Characters?");
+  }
+  
+  passwordText.value = password;
+  
+ 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
